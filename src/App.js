4028d1db6a9f04bb814b3d6dfb4f1b4ad693bb00
Baylospawn <microsoftbaylo@laposte.net>
2025-01-05 12:53:53 +0100

    Persist todo list in localStorage and load saved todos on initialization

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react"; //useState et useEffect sont des hooks qui permettent de gérer l'état et les effets secondaires dans les composants fonctionnels
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  // Ajoute une nouvelle tâche à la liste
-  const addTodo = (text) => {
-    const newTodo = { text, completed: false };
-    setTodos([...todos, newTodo]);
-  };
-  
+  const [todos, setTodos] = useState(() => {
+    // Charge les données depuis le localStorage et donc n'est plus une liste vide comme précédemment
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+    // Ajoute une nouvelle tâche à la liste
+    const addTodo = (text) => {
+      const newTodo = { text, completed: false };
+      const newTodos = [...todos, newTodo];
+      setTodos(newTodos);
+    };
+    
   // Supprime une tâche de la liste
   const deleteTodo = (index) => {
     const newTodos = [...todos];
@@ -24,6 +30,11 @@ function App() {
     newTodos[index].completed = !newTodos[index].completed;
     setTodos(newTodos);
   };
+
+  // Sauvegarde les données dans le localStorage à chaque mise à jour des tâches
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   // Affiche le formulaire et la liste des tâches
   return (
